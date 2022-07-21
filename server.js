d81120aef0c298d37ee173e711f73f58374be8e7
Thilip Dhanavel <87736175+thilipd@users.noreply.github.com>
2022-07-22 04:48:42 +0530

    test

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: `${process.env.CLIENT_URL}`,
+}));
 
 const userRoute = require('./routes/user');
 const movieRoute = require('./routes/movies');
